dec3addabf2a0efe714f4011157f862992e54530
Utomo Yoga <utomoyogafirmansyah@gmail.com>
2023-03-25 17:07:43 +0700

    update feature
    add rating star
    optimize footer and button

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -38,30 +38,34 @@ class Gallery extends HTMLElement {
                     <div>${resto.name}</div>
                   </a>
                   <div>
-                    <svg class="" viewBox="0 0 128 24" width="68" height="12" aria-label="">
-                ${Array(Math.floor(resto.rating))
-                  .fill()
-                  .map((_, i) => {
-                    return `
-                      <path  fill="#FFC107" d="M 12 0C5.388 0 0 5.388 0 12s5.388 12 12 12 12-5.38 12-12c0-6.612-5.38-12-12-12z" transform="translate(${26 * i} 0)"></path>
-                    `;
-                  })
-                  .join("")}
-                ${
-                  resto.rating % 1 !== 0
-                    ? `
-                      <g transform="translate(${26 * Math.floor(resto.rating)} 0)">
-                        <clipPath id="clip-rating">
-                          <rect x="0" y="0" width="${(resto.rating % 1) * 100}%" height="100%"/>
-                        </clipPath>
-                        <g clipPath="url(#clip-rating)">
-                          <path fill="#FFC107" d="M12 0C5.389 0 0 5.389 0 12c0 6.62 5.389 12 12 12 6.62 0 12-5.379 12-12S18.621 0 12 0zm0 2a9.984 9.984 0 0110 10 9.976 9.976 0 01-10 10z"></path>
+                  <svg class="" viewBox="0 0 128 24" width="68" height="12" aria-label="">
+                  ${Array(Math.floor(resto.rating))
+                    .fill()
+                    .map((_, i) => {
+                      return `
+                        <path  fill="#FFC107" d="M22 10.1c.1-.5-.3-1.1-.8-1.1l-5.7-.8L12.9 3c-.1-.2-.2-.3-.4-.4-.5-.3-1.1-.1-1.4.4L8.6 8.2 2.9 9c-.3 0-.5.1-.6.3-.4.4-.4 1 0 1.4l4.1 4-1 5.7c0 .2 0 .4.1.6.3.5.9.7 1.4.4l5.1-2.7 5.1 2.7c.1.1.3.1.5.1h.2c.5-.1.9-.6.8-1.2l-1-5.7 4.1-4c.2-.1.3-.3.3-.5z" transform="translate(${
+                          26 * i
+                        } 0)"></path>
+                      `;
+                    })
+                    .join("")}
+                  ${
+                    resto.rating % 1 !== 0
+                      ? `
+                        <g transform="translate(${26 * Math.floor(resto.rating)} 0)">
+                          <clipPath id="clip-rating">
+                            <rect x="0" y="0" width="${(resto.rating % 1) * 100}%" height="100%"/>
+                          </clipPath>
+                          <g clipPath="url(#clip-rating)">
+                            <path fill="#FFC107" d="M22 10.1c.1-.5-.3-1.1-.8-1.1l-5.7-.8L12.9 3c-.1-.2-.2-.3-.4-.4-.5-.3-1.1-.1-1.4.4L8.6 8.2 2.9 9c-.3 0-.5.1-.6.3-.4.4-.4 1 0 1.4l4.1 4-1 5.7c0 .2 0 .4.1.6.3.5.9.7 1.4.4l5.1-2.7 5.1 2.7c.1.1.3.1.5.1h.2c.5-.1.9-.6.8-1.2l-1-5.7 4.1-4c.2-.1.3-.3.3-.5z"></path>
+                          </g>
                         </g>
-                      </g>
-                      `
-                    : ""
-                }
-              </svg>
+                        `
+                      : ""
+                  }
+                </svg>
+                
+                
                 <span>${resto.rating}</span>
               </div>
                 <div>${resto.city}</div>
