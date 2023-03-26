107ef65222aecdec2609f0f798d08522cbab3b7d
Utomo Yoga <utomoyogafirmansyah@gmail.com>
2023-03-26 22:41:50 +0700

    update feature

--- src/components/recomendation.js
+++ src/components/recomendation.js
@@ -22,7 +22,7 @@ class Recomendation extends HTMLElement {
                 <div class="img-resto-recomen">
                   <img src="${resto.pictureId}" alt="${resto.name}" />
                   <button id="myButton">
-                    <svg viewBox="0 0 24 24" width="24px" height="24px" class="">
+                    <svg viewBox="0 0 24 24"  class="">
                       <path
                         fill="#ffffff"
                         d="M19.665 6.346a5.018 5.018 0 00-3.417-1.34c-1.27 0-2.491.478-3.417 1.34l-.84.802-.859-.812A5.019 5.019 0 007.72 5a5.019 5.019 0 00-3.412 1.336c-1.899 1.773-1.579 4.508-.01 6.43A101.233 101.233 0 0011.99 20s5.995-5.143 7.674-7.224c1.679-2.08 1.879-4.657 0-6.43z"
